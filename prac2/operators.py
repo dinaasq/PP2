619851f504a2d14b18c8ab90fea4537d4d86924f
Dina <d_erbulat@kbtu.kz>
2026-02-06 00:19:15 +0500

    c3

--- prac2/operators.py
+++ prac2/operators.py
@@ -1,7 +1,7 @@
-sum1 = 100 + 50     
-sum2 = sum1 + 250    
-sum3 = sum2 + sum2   
-print(sum1)
-print(sum2)
-print(sum3)
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
 
+print(x is z)
+print(x is y)
+print(x == y)
